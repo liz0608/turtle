78ebe8cce22f7844af764d1c22db9c98f9996944
liz0608 <liz0608@gmail.com>
2015-05-09 16:10:57 +0800

    modify package reference

--- src/main/java/com/turtle/www/dao/impl/UserDao.java
+++ src/main/java/com/turtle/www/dao/impl/UserDao.java
@@ -8,11 +8,11 @@ import com.turtle.www.domain.User;
 public class UserDao extends SqlSessionDaoSupport implements IUserDao {
 	public User getUserByName(User user) {
 		return (User) getSqlSession().selectOne(
-				"com.webdev.www.dao.impl.UserDao.getUserByName", user);
+				"com.turtle.www.dao.impl.UserDao.getUserByName", user);
 	}
 
 	public int insertUser(User user) {
 		return getSqlSession().insert(
-				"com.webdev.www.dao.impl.UserDao.insertUser", user);
+				"com.turtle.www.dao.impl.UserDao.insertUser", user);
 	}
 }
